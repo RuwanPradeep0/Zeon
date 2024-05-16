dc3829674689261e37ab36c4ed030d7a66d72826
Ruwan Pradeep <ruwanpradeep9911@gmail.com>
2024-05-16 19:21:31 +0530

    opening screen

---
+++ app/profile.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>profile 21121211</Text>
+    </View>
+  )
+}
+
+export default Profile
